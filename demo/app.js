6c0553a6fdd2a576f7bb126e14b40e186befa1bb
Tony Narlock <tony@git-pull.com>
2021-05-03 13:48:14 -0500

    Demo: Live server / dev loop

---
+++ demo/app.js
@@ -0,0 +1,19 @@
+import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
+
+import Math from "../src/math";
+
+ClassicEditor.create(document.querySelector("#editor"), {
+	plugins: [Essentials, Paragraph, Bold, Italic, Math],
+	toolbar: ["bold", "italic", "math"],
+	math: { engine: "katex" },
+})
+	.then((editor) => {
+		console.log("Editor was initialized", editor);
+	})
+	.catch((error) => {
+		console.error(error.stack);
+	});
