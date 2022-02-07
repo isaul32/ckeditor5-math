7e788e383c78842ae81bc0ca17770f9499d82f8d
Tony Narlock <tony@git-pull.com>
2022-02-07 15:37:29 -0600

    v32.0.0 package and webpack updates, node 12 -> 14

--- webpack.config.js
+++ webpack.config.js
@@ -44,14 +44,16 @@ module.exports = {
 					"css-loader",
 					{
 						loader: "postcss-loader",
-						options: styles.getPostCssConfig({
-							themeImporter: {
-								themePath: require.resolve(
-									"@ckeditor/ckeditor5-theme-lark"
-								),
-							},
-							minify: true,
-						}),
+						options: {
+							postcssOptions: styles.getPostCssConfig({
+								themeImporter: {
+									themePath: require.resolve(
+										"@ckeditor/ckeditor5-theme-lark"
+									),
+								},
+								minify: true,
+							}),
+						},
 					},
 				],
 			},
