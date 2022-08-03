4205f1f0be113316381ee34a3078bc61d3eaadc3
Tony Narlock <tony@git-pull.com>
2022-08-03 18:18:34 -0500

    demo: Add example of katexRenderOptions

--- demo/app.js
+++ demo/app.js
@@ -3,18 +3,25 @@ import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
 import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
 import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
 import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
-import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
+import CKEditorInspector from "@ckeditor/ckeditor5-inspector";
 
 import Math from "../src/math";
 
 ClassicEditor.create(document.querySelector("#editor"), {
 	plugins: [Essentials, Paragraph, Bold, Italic, Math],
 	toolbar: ["bold", "italic", "math"],
-	math: { engine: "katex" },
+	math: {
+		engine: "katex",
+		katexRenderOptions: {
+			macros: {
+				"\\test": "\\mathrel{\\char`≠}",
+			},
+		},
+	},
 })
 	.then((editor) => {
 		console.log("Editor was initialized", editor);
-		CKEditorInspector.attach(editor)
+		CKEditorInspector.attach(editor);
 	})
 	.catch((error) => {
 		console.error(error);
