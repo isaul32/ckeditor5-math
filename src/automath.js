fbf6a9339414a79058cec0b88c2a4758891364a0
Luca Marius Cobzaru <e11712226@student.tuwien.ac.at>
2022-09-08 13:17:11 +0200

    STU-1225 make multiline operations undo work for everything except when it is in text, implement feature to not break editor when that happens

--- src/automath.js
+++ src/automath.js
@@ -1,220 +1,155 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import Undo from '@ckeditor/ckeditor5-undo/src/undo';
-import LiveRange from '@ckeditor/ckeditor5-engine/src/model/liverange';
 import LivePosition from '@ckeditor/ckeditor5-engine/src/model/liveposition';
 import global from '@ckeditor/ckeditor5-utils/src/dom/global';
 
-const mathKeystroke = 'Shift+4';
-
 import {
-	extractDelimiters,
 	hasDelimiters,
 	delimitersCounts,
 	getMathFormsAndText,
 	delimitersAreMatching,
-	openingBrackets, closingBrackets, makeFormulas
+	makeFormulas
 } from './utils';
-import {elementAt} from "rxjs";
+import preventDefault from "@ckeditor/ckeditor5-ui/src/bindings/preventdefault";
 
 export default class AutoMath extends Plugin {
 	static get requires() {
-		return [ Undo ];
+		return [Undo];
 	}
 
 	static get pluginName() {
 		return 'AutoMath';
 	}
 
-	constructor( editor ) {
-		super( editor );
+	constructor(editor) {
+		super(editor);
 
 		this._timeoutId = null;
 
-		this._positionToInsert = null;
+		this._notUndoableOperation = -1;
 	}
 
 	init() {
 		const editor = this.editor;
 		const modelDocument = editor.model.document;
-		const view = editor.editing.view;
 		//change < Clipboard > to < 'ClipboardPipeline' > because in version upgrade from 26 to 27
 		//the usage of this call changed
-		console.log('1');
-		this.listenTo( editor.plugins.get( 'ClipboardPipeline' ), 'inputTransformation', (evt, data) => {
+		this.listenTo(editor.plugins.get('ClipboardPipeline'), 'inputTransformation', (evt, data) => {
 
-			const dataTransfer = data.dataTransfer;
-			console.log('2');
+			if (this._notUndoableOperation !== -1) {
+				this._notUndoableOperation += 1;
+			}
 
 			const firstRange = modelDocument.selection.getFirstRange();
-			document.firstRange = firstRange;
-			const allRanges = modelDocument.selection.getRanges();
-			console.log(allRanges);
-			for (let a of allRanges) {
-				console.log(a);
-			}
-			document.allRanges = allRanges;
 
-			//let content = data.content || '';
-			if ( dataTransfer.getData( 'text/html' ) ) {
-				console.log('html');
-				//content = normalizeClipboardHtml( dataTransfer.getData( 'text/html' ) );
-			} else if ( dataTransfer.getData( 'text/plain' ) ) {
-				console.log('plain');
-				//content = plainTextToHtml( dataTransfer.getData( 'text/plain' ) );
-			}
+			const leftPosition = firstRange.start;
+			leftPosition.stickiness = 'toPrevious';
 
-			console.log(modelDocument.selection);
-			document.aa = modelDocument.selection;
-			const leftLivePosition = LivePosition.fromPosition( firstRange.start );
-			leftLivePosition.stickiness = 'toPrevious';
+			const rightPosition = firstRange.end;
+			rightPosition.stickiness = 'toNext';
 
 			const rightLivePosition = LivePosition.fromPosition( firstRange.end );
 			rightLivePosition.stickiness = 'toNext';
+			console.log(leftPosition);
+			console.log(rightPosition);
+			console.log(rightLivePosition);
 
-			modelDocument.once( 'change:data', () => {
-				console.log('3');
-				this._mathBetweenPositions( leftLivePosition, rightLivePosition );
-
-				leftLivePosition.detach();
+			modelDocument.once('change:data', () => {
+				this._mathBetweenPositions(leftPosition, rightPosition, rightLivePosition);
 				rightLivePosition.detach();
-			}, { priority: 'high' } );
-		} );
+			}, {priority: 'high'});
+		});
 
-		editor.commands.get( 'undo' ).on( 'execute', () => {
-			console.log('4');
-			if ( this._timeoutId ) {
-				global.window.clearTimeout( this._timeoutId );
-				this._positionToInsert.detach();
+
+		editor.commands.get('undo').on('execute', (evt, data) => {
+			if (this._timeoutId) {
+				global.window.clearTimeout(this._timeoutId);
 
 				this._timeoutId = null;
-				this._positionToInsert = null;
 			}
-		}, { priority: 'high' } );
-
-
-/*		editor.keystrokes.set( mathKeystroke, ( keyEvtData, cancel ) => {
-			// Prevent focusing the search bar in FF and opening new tab in Edge. #153, #154.
-			cancel();
-
-			const modelDocument = editor.model.document;
-			const allRanges = modelDocument.selection.getRanges();
-			for (let range of allRanges) {
-				console.log(range.start);
-				console.log(range.end);
+			if (this._notUndoableOperation === 0) {
+				// Stop executing next callbacks.
+				evt.stop();
+				//evt.cancel();
+			} else if (this._notUndoableOperation !== -1) {
+				this._notUndoableOperation -= 1;
 			}
-		} );*/
+		}, {priority: 'high'});
 	}
 
 
-	_mathBetweenPositions( leftPosition, rightPosition ) {
-		console.log('5');
+	_mathBetweenPositions(leftPosition, rightPosition, rightLivePosition) {
 		const editor = this.editor;
+		let isAtStartOrEndOfText = false;
+		let isMultiLine = false;
 
-		const mathConfig = this.editor.config.get( 'math' );
-
-		console.log(leftPosition);
-		console.log(rightPosition);
-
-		const equationRange = new LiveRange( leftPosition, rightPosition );
-		const walker = equationRange.getWalker( { ignoreElementEnd: true } );
-
-		let text = '';
-
-		document.walker = walker;
-
-		let noMath = true;
-		// Get equation text
-		for ( const node of walker ) {
-			console.log(node);
-			// Skip if don't have delimiters
-			if (  node.item.is( '$textProxy' ) && hasDelimiters( node.item.data ) && delimitersCounts( node.item.data ) % 2 === 0 ) {
-				noMath = false;
-			}
-		}
-
-		//return if no node has any math
-		if (noMath) {
-			return;
-		}
-
-		const mathCommand = editor.commands.get( 'math' );
-
+		const mathCommand = editor.commands.get('math');
+		const mathConfig = this.editor.config.get('math');
 		// Do not anything if math element cannot be inserted at the current position
-		if ( !mathCommand.isEnabled ) {
+		if (!mathCommand.isEnabled) {
 			return;
 		}
-		console.log('8');
-		this._positionToInsert = LivePosition.fromPosition( leftPosition, {stickiness: 'toNone'} );
+
+		if ( leftPosition.path[leftPosition.path.length -1] === 0 ||
+			(leftPosition.path[0] === rightLivePosition.path[0] && leftPosition.path[1] === rightLivePosition.path[1])) {
+			isAtStartOrEndOfText = true;
+		}
+		// if the position is at the start,
+		rightPosition = editor.model.document.selection.getFirstRange().end;
+		rightPosition.stickiness = 'toNext';
+
 		// With timeout user can undo conversation if want use plain text
-		this._timeoutId = global.window.setTimeout( () => {
-			console.log('9');
-			editor.model.change( writer => {
-				this._timeoutId = null;
-
-				let insertPosition;
-
-				// Check if position where the math element should be inserted is still valid.
-				if ( this._positionToInsert.root.rootName !== '$graveyard' ) {
-					insertPosition = this._positionToInsert;
-				}
-				console.log('10');
-
-				let realLeftPosition = leftPosition;
-				let realRightPosition = null;
-
-				let walkerino = equationRange.getWalker( { ignoreElementEnd: true } );
+		this._timeoutId = global.window.setTimeout(() => {
+			if (this._notUndoableOperation !== -1) {
+				this._notUndoableOperation += 1;
+			}
+			this._timeoutId = null;
+			editor.model.change(writer => {
+				const equationRange = writer.createRange(leftPosition, rightPosition);
+				let walker = equationRange.getWalker({ignoreElementEnd: true});
 				let nodeArray = [];
-				for ( const node of walkerino ) {
-					console.log(node);
+				for (const node of walker) {
 					nodeArray.push(node);
+				}
+				if (nodeArray.length > 1) {
+					isMultiLine = true;
 				}
 
 				for (let node of nodeArray) {
-					text = node.item.data;
-					console.log(text);
-					let mathFormsAndText = getMathFormsAndText( text );
 
-					if ( node.item.is( '$textProxy' ) && hasDelimiters( text ) && delimitersCounts( text ) % 2 === 0 &&
-						mathFormsAndText !== undefined && delimitersAreMatching( mathFormsAndText ) ) {
+					let text = node.item.data;
+					let mathFormsAndText = getMathFormsAndText(text);
+					if (node.item.is('$textProxy') && hasDelimiters(text) && delimitersCounts(text) % 2 === 0 &&
+						mathFormsAndText !== undefined && delimitersAreMatching(mathFormsAndText)) {
 
-					let finishedFormulas = makeFormulas( mathFormsAndText );
+						let finishedFormulas = makeFormulas(mathFormsAndText);
 
-					editor.model.change(innerWriter => {
-						console.log('math');
-
-						console.log(realLeftPosition);
-						console.log(realRightPosition);
-						const realRange = writer.createRange(node.previousPosition,  node.nextPosition);
-
+						const realRange = writer.createRange(node.previousPosition, node.nextPosition);
 						writer.remove(realRange);
-
-						realLeftPosition = node.previousPosition;
 
 						let mathElement;
 						for (let i = finishedFormulas.length - 1; i >= 0; i--) {
 							if (i % 2 === 0) {
-								console.log('insertText');
-								writer.insertText(finishedFormulas[i], realLeftPosition);
+								writer.insertText(finishedFormulas[i], node.previousPosition);
 							} else {
-								console.log('insertMath');
 								let params = Object.assign(finishedFormulas[i], {
 									type: mathConfig.outputType
 								});
 								mathElement = writer.createElement(params.display ? 'mathtex-display' : 'mathtex-inline', params);
-								editor.model.insertContent(mathElement, realLeftPosition);
+								editor.model.insertContent(mathElement, node.previousPosition);
 							}
 						}
 
 						if (finishedFormulas.length === 3 && finishedFormulas[0].trim() === '' && finishedFormulas[2].trim() === '') {
-							innerWriter.setSelection(mathElement, 'on');
+							writer.setSelection(mathElement, 'on');
 						}
-					});
-				} }
-			} );
-			this._positionToInsert.detach();
-			this._positionToInsert = null;
-			//don't make mathui appear after clicking after pasting formula into editor
+						if (!isAtStartOrEndOfText && isMultiLine) {
+							this._notUndoableOperation = 1;
+						}
+					}
+				}
+			});
 			mathCommand.resetMathCommand();
-		}, 100 );
+		}, 100);
 	}
 }
---
+++ src/test.js
@@ -0,0 +1,115 @@
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import Undo from '@ckeditor/ckeditor5-undo/src/undo';
+import LiveRange from '@ckeditor/ckeditor5-engine/src/model/liverange';
+import LivePosition from '@ckeditor/ckeditor5-engine/src/model/liveposition';
+import global from '@ckeditor/ckeditor5-utils/src/dom/global';
+
+export default class Test extends Plugin {
+	static get requires() {
+		return [Undo];
+	}
+
+	static get pluginName() {
+		return 'Test';
+	}
+
+	constructor(editor) {
+		super(editor);
+		this._timeoutId = null;
+		this._positionToInsert = null;
+	}
+
+	init() {
+		const editor = this.editor;
+		const modelDocument = editor.model.document;
+		const view = editor.editing.view;
+		//change < Clipboard > to < 'ClipboardPipeline' > because in version upgrade from 26 to 27
+		//the usage of this call changed
+		this.listenTo(editor.plugins.get('ClipboardPipeline'), 'inputTransformation', (evt, data) => {
+			const firstRange = modelDocument.selection.getFirstRange();
+
+			const leftLivePosition = firstRange.start;
+			leftLivePosition.stickiness = 'toPrevious';
+
+			const rightLivePosition = firstRange.end;
+			rightLivePosition.stickiness = 'toNext';
+
+
+			modelDocument.once('change:data', () => {
+				this._boldBetweenPositions(leftLivePosition, rightLivePosition);
+			}, {priority: 'high'});
+		});
+
+		editor.commands.get('undo').on('execute', () => {
+			if (this._timeoutId) {
+				global.window.clearTimeout(this._timeoutId);
+
+				this._timeoutId = null;
+			}
+		}, {priority: 'high'});
+	}
+
+
+	_boldBetweenPositions(leftPosition, rightPosition) {
+
+		const editor = this.editor;
+		if ( editor.model.document.selection.isCollapsed ) {
+			console.log('isCollapsed');
+		}
+
+		// if the position is at the start,
+		rightPosition = editor.model.document.selection.getFirstRange().end;
+		rightPosition.stickiness = 'toNext';
+
+
+
+		// With timeout user can undo conversation if wants to use plain text
+		this._timeoutId = global.window.setTimeout(() => {
+			this._timeoutId = null;
+
+			editor.model.change(writer => {
+				const equationRange = writer.createRange(leftPosition, rightPosition);
+
+				let walker = equationRange.getWalker({ignoreElementEnd: true});
+				let nodeArray = [];
+				for (const node of walker) { // remember nodes, because when they are changed model-textproxy-wrong-length error occurs
+					nodeArray.push(node);
+				}
+				for (let node of nodeArray) {
+					let text = node.item.data;
+					if (node.item.is('$textProxy') && text !== undefined && text.match(/&/g)) {
+
+
+						let finishedFormulas = this._split(text);
+
+
+						const realRange = writer.createRange(node.previousPosition, node.nextPosition);
+						writer.remove(realRange);
+
+						for (let i = finishedFormulas.length - 1; i >= 0; i--) {
+							if (i % 2 === 0) {
+								writer.insertText(finishedFormulas[i], node.previousPosition);
+							} else {
+								writer.insertText(finishedFormulas[i], {bold: true}, node.previousPosition);
+							}
+						}
+					}
+				}
+			});
+		}, 100);
+	}
+
+	_split(text) {
+		let mathFormsAndText = text.split(/(&)/g);
+		let mathTextArray = [];
+		for (let i = 0; i < mathFormsAndText.length; i++) {
+			if (i % 4 === 0) {
+				mathTextArray.push(mathFormsAndText[i]);
+
+			} else if (i % 2 === 0) {
+				mathTextArray.push(mathFormsAndText[i]);
+			}
+		}
+		return mathTextArray;
+	}
+}
