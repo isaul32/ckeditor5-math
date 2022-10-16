e57c46a96bc502189672266a0677606052d2dd5f
Luca Marius Cobzaru <e11712226@student.tuwien.ac.at>
2022-10-16 16:57:07 +0200

    STU-1225 make shortcut press not add to the end of the input field, but where focus is

--- src/ui/shortcutsview.js
+++ src/ui/shortcutsview.js
@@ -2,6 +2,7 @@ import { ListView } from '@ckeditor/ckeditor5-ui';
 import View from '@ckeditor/ckeditor5-ui/src/view';
 import { groupBy } from 'lodash';
 import svgIcons from '../../assets/math-shortcut-icons';
+import global from "@ckeditor/ckeditor5-utils/src/dom/global";
 
 // choosen category
 let activeCategory = 'Allgemein';
@@ -37,13 +38,14 @@ const categories = Object.keys(groupedCommands).map(cat => {
     p.innerText = cat;
     if(p.id == activeCategory) p.classList.add('active');
 
-    p.onclick = () => {
-        activeCategory = p.innerText;
-        document.querySelector('.active')?.classList.remove('active');
-        p.classList.add('active');
-
-        renderCommands(activeCategory);
-    }
+    p.onmousedown = (e) => {
+		e.preventDefault();
+		activeCategory = p.innerText;
+		document.querySelector('.active')?.classList.remove('active');
+		p.classList.add('active');
+		renderCommands(activeCategory);
+		//inputField.inputView.focus();
+	}
 
     return p;
 });
@@ -53,24 +55,76 @@ const categories = Object.keys(groupedCommands).map(cat => {
 const ul = document.createElement('ul');
 ul.id = 'commands-list';
 ul.classList.add('commands-list');
+
 const renderCommands = (category) => {
-    ul.innerHTML = '';
+	ul.innerHTML = '';
 
-    for (const command of groupedCommands[category]) {
-        const li = document.createElement('li');
+	for (const command of groupedCommands[category]) {
+		const li = document.createElement('li');
 
-        li.onclick = () => {
-            document.querySelector('#math-input-field').textContent += command.element.id;
-            inputField.inputView.fire('input');
+		li.onclick = () => {
+			pasteHtmlAtCaret(command.element.id, false); //todo check if better true or false
+			renderCommands(activeCategory);
+		}
+		li.appendChild(command.element);
+		ul.appendChild(li);
+	}
 
-            renderCommands(activeCategory);
-        }
+	return ul;
+}
 
-        li.appendChild(command.element);
-        ul.appendChild(li);
-    }
+// copied from https://stackoverflow.com/questions/6690752/insert-html-at-caret-in-a-contenteditable-div
+function pasteHtmlAtCaret(html, selectPastedContent) {
+	var sel, range;
+	if (selectionIsInMath()) {
+		// IE9 and non-IE
+		sel = window.getSelection();
+		if (sel.getRangeAt && sel.rangeCount) {
+			range = sel.getRangeAt(0);
+			range.deleteContents();
 
-    return ul;
+			// Range.createContextualFragment() would be useful here but is
+			// only relatively recently standardized and is not supported in
+			// some browsers (IE9, for one)
+			var el = document.createElement("div");
+			el.innerHTML = html;
+			var frag = document.createDocumentFragment(), node, lastNode;
+			while ( (node = el.firstChild) ) {
+				lastNode = frag.appendChild(node);
+			}
+			var firstNode = frag.firstChild;
+			range.insertNode(frag);
+
+			// Preserve the selection
+			if (lastNode) {
+				range = range.cloneRange();
+				range.setStartAfter(lastNode);
+				if (selectPastedContent) {
+					range.setStartBefore(firstNode);
+				} else {
+					range.collapse(true);
+				}
+				sel.removeAllRanges();
+				sel.addRange(range);
+			}
+			inputField.inputView.fire('input');
+		}
+	} else { // if selection is not in math input, just append it to the field
+		document.querySelector('#math-input-field').textContent += html;
+		inputField.inputView.fire('input');
+	}
+}
+
+//check if selection is in input: if yes then
+function selectionIsInMath() {
+	return window.getSelection &&
+		window.getSelection().anchorNode && ((
+				window.getSelection().anchorNode.id &&
+				window.getSelection().anchorNode.id === 'math-input-field')
+			||
+				(window.getSelection().anchorNode.parentNode &&
+				window.getSelection().anchorNode.parentNode.id &&
+				window.getSelection().anchorNode.parentNode.id === 'math-input-field'));
 }
 
 // ui wrapper
